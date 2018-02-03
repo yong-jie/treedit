9322a841af4a081e745019faa01f963bfff7da10
SoraSkyy <viorama@gmail.com>
2018-02-04 03:14:25 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import path from 'path';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import routes from './routes';
+
+const app = express();
+app.disable('x-powered-by');
+
+// View engine setup
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev', {
+  skip: () => app.get('env') === 'test',
+}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Routes
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.use('/api', routes);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handler
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  res
+    .status(err.status || 500)
+    .render('error', {
+      message: err.message,
+    });
+});
+
+export default app;
