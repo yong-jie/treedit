249c63396dcca985b82e9e0c7ba008df26031c3d
SoraSkyy <viorama@gmail.com>
2018-02-06 00:51:15 +0800

    Add api for topic list and topic create, with tests

---
+++ test/api.test.js
@@ -0,0 +1,50 @@
+import request from 'supertest';
+import app from '../src/app';
+
+describe('GET /', () => {
+  it('should render properly', async () => {
+    await request(app).get('/').expect(200);
+  });
+});
+
+describe('GET /api/topic/list/1', () => {
+  it('should fetch a blank list of topics', async () => {
+    const response = await request(app).get('/api/topic/list/1');
+    const { success, result } = response.body;
+    expect(success).toBe(true);
+    expect(result.length).toBe(0);
+  });
+});
+
+describe('POST /api/topic/create', () => {
+  it('should successfully create a topic and list it correctly', async () => {
+    const response = await request(app)
+      .post('/api/topic/create')
+      .type('form')
+      .send({ message: 'Hello' })
+      .set('Accept', /application\/json/);
+    const { success, result } = response.body;
+    expect(success).toBe(true);
+    expect(result.length).toBe(1);
+    expect(result[0].id).toBe(1);
+    expect(result[0].message).toBe('Hello');
+    expect(result[0].score).toBe(0);
+  });
+
+  it('should successfully create a second topic and list it correctly', async () => {
+    const response = await request(app)
+      .post('/api/topic/create')
+      .type('form')
+      .send({ message: 'Hello2' })
+      .set('Accept', /application\/json/);
+    const { success, result } = response.body;
+    expect(success).toBe(true);
+    expect(result.length).toBe(2);
+    expect(result[0].id).toBe(1);
+    expect(result[0].message).toBe('Hello');
+    expect(result[0].score).toBe(0);
+    expect(result[1].id).toBe(2);
+    expect(result[1].message).toBe('Hello2');
+    expect(result[1].score).toBe(0);
+  });
+});
