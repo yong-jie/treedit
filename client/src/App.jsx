764852ba31cf0a13067e901f2a368ae99fd975ab
SoraSkyy <viorama@gmail.com>
2018-02-09 02:17:42 +0800

    Add basic topic listing functionality in frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import TopicList from './Topic/TopicList';
+import { fetchTopics } from './api';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      topics: [],
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveTopics(1);
+  }
+
+  retrieveTopics(page) {
+    fetchTopics(page)
+      .then(response => this.setState({
+        topics: response.body.result,
+      }));
+  }
+
   render() {
     return (
       <div className="App">
         <header>
           <p>Header</p>
         </header>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TopicList topics={this.state.topics} />
       </div>
     );
   }
